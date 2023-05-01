e138df761d3d9d50396624a94144a65bf4714399
Salma Magdy Ishak <salma.m.ishakk@gmail.com>
2023-05-01 09:10:37 +0300

    filtered  home table of bills added

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -5,36 +5,80 @@ import { CompletedBills } from '../classes/CompletedBills';
 import { User } from '../classes/user';
 import { DueBills } from '../classes/DueBills';
 import { HttpServiceService } from '../Services/http-service.service';
+import { FormControl } from '@angular/forms';
+import { Observable, startWith , map} from 'rxjs';
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent {
-  DueBills: any[] = [];
-
+  Bills: any[] = [];
+  name :string='';
+  filterDueDate = '';
+  filterStatus = '';
+  filterService = '';
+  Dates: string[] = [];
   CompletedBills: any[] = [];
   user12: any;
-  user1:any;
+  user1: any;
+
   constructor(
     private userService: UsersDataService,
     private userDataService: UserdataService,
     private httpservice: HttpServiceService
   ) {}
 
+  incomeControl = new FormControl();
+  filteredValues: Observable<string[]> | undefined;
+
   ngOnInit() {
     this.user1 = this.userDataService.user;
+    this.name = this.user1.firstname;
+    for (const bill of this.user1.waterBills) {
+      if (bill.amount !== 0 && bill !== null) {
+        this.Bills.push({ ...bill, service: 'Water' });
+        this.Dates.push(bill.date);
+      }
+    }
+    for (const eB of this.user1.electricityBills) {
+      if (eB.amount !== 0 && eB !== null)
+        this.Bills.push({ ...eB, service: 'Electricity' });
+    }
+    for (const tA of this.user1.telephoneBills) {
+      if (tA.amount !== 0 && tA !== null)
+        this.Bills.push({ ...tA, service: 'Telephone' });
+    }
+    console.log(this.Bills);
+    this.filteredValues = this.incomeControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this.filter(value))
+    );
+  }
 
-   // this.user1.DueBills.forEach((bill: DueBills) => {
-    //  if (bill != null && bill.service != '') {
-    //    this.DueBills.push(bill);
-    //  }
-    //});
+  filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.Bills.filter((income) =>
+      income.toLowerCase().includes(filterValue)
+    );
+  }
 
-    //this.user1.CompletedBills.forEach((bill: CompletedBills) => {
-     // if (bill != null && bill.service != '') {
-     //   this.CompletedBills.push(bill);
-     // }
-  //  });
+  get filteredBills() {
+    return this.Bills.filter((bill) => {
+      if (this.filterDueDate && bill.date !== this.filterDueDate) {
+        return false; // filter out bills with different due date
+      }
+      if (this.filterService && bill.service !== this.filterService) {
+        return false; // filter out bills with different service
+      }
+      if (this.filterStatus === 'Paid' && !bill.isDue) {
+        return false; // filter out bills that are not completed
+      }
+      if (this.filterStatus === 'Due' && bill.isDue) {
+        return false; // filter out bills that are completed
+      }
+      return true; // keep bills that pass all filters
+    });
   }
 }
