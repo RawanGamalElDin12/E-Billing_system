d5119368367e877905b651ebd82060a30b1ae352
Salma Magdy Ishak <salma.m.ishakk@gmail.com>
2023-05-02 10:20:42 +0300

    added payment to water service

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,44 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsersDataService } from '../Services/users-data.service';
 import { BillingServiceService } from '../Services/billing-service.service';
 import { HttpServiceService } from '../Services/http-service.service';
 import { UserdataService } from '../Services/userdata.service';
 import { customer } from '../classes/customer';
 import { WaterBill } from '../classes/bill';
+import { PayServiceService } from '../Services/pay-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.css'],
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit{
   selectedOption: string = '';
   cash: boolean = false;
   initialize: boolean = true;
   credit: boolean = false;
-  // bill: { userId: number; Service: string; date: string; amount: number } = {
-  //   userId: 222222222,
-  //   Service: 'water-bill',
-  //   date: '10-4-2023',
-  //   amount: 60,
-  // };
+  service:string='';
+  payType: string='';
+  creditPaid= false;
+  invoice =0;
+
+  ngOnInit(): void {
+    this.bill = this.user.waterBills.filter(bill=> bill.billid==this.payServ.billid)[0] ;
+        this.service= this.payServ.serviceType;
+        this.invoice = this.invoiceNumber();
+  }
   constructor(
     private billingservice: BillingServiceService,
     private userdataService: UserdataService,
-    private http: HttpServiceService
+    private http: HttpServiceService,
+    private payServ : PayServiceService,
+    private router :Router
   ) {
     this.user = this.userdataService.user;
   }
   user: customer;
-  bill: WaterBill[] = [];
+  bill: WaterBill = {
+    billid: 1222222222222222222,
+    amount: 0,
+    date: '',
+    status: '',
+    lateFees: 0,
+    consumption: 0,
+    paymentType: '',
+  };
 
   onClick() {
     if (this.selectedOption === 'cash') {
       this.initialize = false;
       this.cash = true;
+      this.payType='cash';
+      
     } else if (this.selectedOption === 'credit') {
       this.initialize = false;
       this.credit = true;
+      this.payType='credit';
     }
+    // console.log(this.user.waterBills[this.payServ.billid]);
+    // this.user.waterBills.push(water);
+   
+   
+    // this.user.waterBills[this.payServ.billid].status = 'Paid';
+    this.user.waterBills.filter(bill=> bill.billid==this.payServ.billid)[0].status = 'Paid';
+    // this.user.waterBills[this.payServ.billid].paymentType=this.payType;
+    this.user.waterBills.filter(bill=> bill.billid==this.payServ.billid)[0].paymentType=this.payType;
+    // console.log(this.user.waterBills[this.payServ.billid]);
+
+     this.http.updateUser(this.user).subscribe();
+    this.invoice = this.invoiceNumber();
+
   }
+  paid()
+  {
+    
+
+    this.creditPaid= true;
+  }
+
+  invoiceNumber ()
+  {
+    const min = 10000000000000; // 10^13
+  const max = 99999999999999; // 10^14 - 1
+  return Math.round(Math.random() * (max - min) + min);
+  }
+ 
 }
