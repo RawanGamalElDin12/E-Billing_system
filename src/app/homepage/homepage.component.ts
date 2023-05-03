47ea011849dce34a322d34a184e78b1d2c480e8c
Salma Magdy Ishak <salma.m.ishakk@gmail.com>
2023-05-03 12:59:39 +0300

    added comments to home component code

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -17,52 +17,47 @@ import { CheckLateFeesService } from '../Services/check-late-fees.service';
   styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent {
-  Bills: any[] = [];
-  name: string = '';
-  filterDueDate = '';
-  filterStatus = '';
-  filterService = '';
-  Dates: string[] = [];
-  CompletedBills: any[] = [];
-  user12: any;
-  user1: any;
-  incomeControl = new FormControl();
-  checkLateFees:any;
-  filteredValues: Observable<string[]> | undefined;
+  Bills: any[] = []; // an array to store all bills for the user
+  name: string = ''; // a string to store the user's first name
+  filterDueDate = ''; // a string to store the selected due date filter
+  filterStatus = ''; // a string to store the selected status filter
+  filterService = ''; // a string to store the selected service filter
+  Dates: string[] = []; // an array to store all unique dates for the bills
+  CompletedBills: any[] = []; // an array to store all completed bills for the user
+  user12: any; // a variable to store the user data
+  user1: any; // a variable to store the user data
+  incomeControl = new FormControl(); // a form control for the income filter input
+  checkLateFees:any; // a variable to store the CheckLateFeesService
+
+  filteredValues: Observable<string[]> | undefined; // an observable to store the filtered values for the income filter
 
   constructor(
-
-    private userDataService: UserdataService,
-    private httpservice: HttpServiceService,
-    private payServ: PayServiceService,
-    private router: Router,
-    private checkLate: CheckLateFeesService) {
-    this.user1 = this.userDataService.user;
-    this.name = this.user1.firstname;
-    this.checkLateFees = this.checkLate;
-
+    private userDataService: UserdataService, // a service to get the user data
+    private httpservice: HttpServiceService, // an HTTP service to make requests to the backend
+    private payServ: PayServiceService, // a service to store the payment information for a bill
+    private router: Router, // a service to navigate to different routes
+    private checkLate: CheckLateFeesService // a service to check the late fees for bills
+  ) {
+    this.user1 = this.userDataService.user; // get the user data from the UserDataService
+    this.name = this.user1.firstname; // set the user's first name
+    this.checkLateFees = this.checkLate; // set the CheckLateFeesService
   }
 
   ngOnInit() {
     // adds both electricty bills and water bills in an array togather
     for (const bill of this.user1.waterBills) {
       if (bill.amount !== 0 && bill !== null && bill.date != '') {
-
-
-        //push all bills attributes and add another one service Type
+        // push all bills attributes and add another one service Type
         this.Bills.push({ ...bill, service: 'Water' });
         // push dates of bills in the array 
         if (!this.Dates.includes(bill.date)) {
           this.Dates.push(bill.date);
         }
-
       }
     }
     for (const eB of this.user1.electricityBills) {
       if (eB.amount !== 0 && eB !== null) {
         this.Bills.push({ ...eB, service: 'Electricity' });
-
-
         if (!this.Dates.includes(eB.date)) {
           this.Dates.push(eB.date);
         }
@@ -71,26 +66,24 @@ export class HomepageComponent {
     for (const tA of this.user1.telephoneBills) {
       if (tA.amount !== 0 && tA !== null) {
         this.Bills.push({ ...tA, service: 'Telephone' });
-
-        if (!this.Dates.includes(tA.date)) { this.Dates.push(tA.date); }
+        if (!this.Dates.includes(tA.date)) {
+          this.Dates.push(tA.date);
+        }
       }
     }
 
-    console.log(this.Bills);
+    console.log(this.Bills); // log the bills array to the console
     this.filteredValues = this.incomeControl.valueChanges.pipe(
       startWith(''),
-      map((value) => this.filter(value))
+      map((value) => this.filter(value)) // map the filtered values using the filter method
     );
-
-
   }
-
 
   filter(value: string): string[] {
     const filterValue = value.toLowerCase();
     return this.Bills.filter((income) =>
       income.toLowerCase().includes(filterValue)
-    );
+    ); // filter the bills based on the income filter value
   }
 
   get filteredBills() {
@@ -113,19 +106,15 @@ export class HomepageComponent {
 
   payBill(index: number): void {
     // Pay due bill at the given index
-    this.payServ.billid = this.Bills[index].billid;
-    this.payServ.serviceType = this.Bills[index].service;
-    this.payServ.tarriff = this.Bills[index].tarriff;
-
+    this.payServ.billid = this.Bills[index].billid; // set the bill ID for payment
+    this.payServ.serviceType = this.Bills[index].service; // set the service type for payment
+    this.payServ.tarriff = this.Bills[index].tarriff; // set the tarriff for payment
   }
+
   viewReceipt(index: number) {
+    var id = this.user1.nationalid; // get the user's national ID
+    console.log(id); // log the national ID to the console
 
-    var id = this.user1.nationalid;
-    console.log(id);
-
-    this.router.navigate(['main/receipt', this.Bills[index].billid, id, this.Bills[index].service]);
+    this.router.navigate(['main/receipt', this.Bills[index].billid, id, this.Bills[index].service]); // navigate to the receipt page with the bill ID, national ID, and service type
   }
-
-
-
 }
