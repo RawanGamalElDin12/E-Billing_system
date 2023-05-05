56f38cf87ed607985b93013b3aa4ced1fafd7048
Salma Magdy Ishak <salma.m.ishakk@gmail.com>
2023-05-05 18:41:52 +0300

    fixed bug

--- src/app/service-providers/service-providers.component.ts
+++ src/app/service-providers/service-providers.component.ts
@@ -30,19 +30,22 @@ export class ServiceProvidersComponent implements OnInit {
           // iterate each element in get SPs 
       if (data[key].name!='')
        {  console.log(data[key]);
-        let SP:ServiceProvider = {
+        if (data[key].name!='')
+       { this. SP = {
           id: data[key].id,
           name: data[key].name,
           password: data[key].password,
           tarriff: data[key].tarriff,
           offers:[]
-        };
+        };}
         let offersCards :Offer[]=[];
           for (let offer in data[key].offers) {
-            if (data[key].offers.hasOwnProperty(offer) && data[key].offers[offer].category!=''&&data[key].offers[offer].minutes!=0)
+            if (data[key].offers.hasOwnProperty(offer) && data[key].offers[offer].offerid != 0 && data[key].offers[offer].minutes!=0)
             offersCards.push(data[key].offers[offer]);
+            console.log(offersCards);
           }
-          this.cards.push({...SP,offers:offersCards});
+        
+          this.cards.push({...this.SP ,offers:offersCards});
 
         }}
        
